e95df266a04d7a62666d7c001e9df1c3f5bb8a40
Amad Irfan <99482198+AmadIrfan@users.noreply.github.com>
2024-07-16 16:25:07 +0500

    changes

---
+++ README.md
@@ -0,0 +1,29 @@
+# SASS Pipe with Gulp
+
+### Developer Name
+
+Amad Irfan(2021-CS-25)
+
+### ***Project Name:***
+
+Gulp pipe Implementation for SASS project
+
+### *Run project*
+
+1. Run `npm I` to make package of project able to run.
+
+2. run `npm start` or `npm run start` to run project.
+
+3. **From GitHub Repository Method:**
+
+    To clone repository go on GitHub and copy http link. Go to visual studio and run this command:
+
+   ```bash
+   git clone https://github.com/AmadIrfan/sass-pipeline.git
+   ```
+
+   Now to run  `npm start` to run project.
+
+4. **From Extract Zip-File Method:**
+
+    Extract zip file and now to  `npm I` to run project. Project run successfully.
--- gulpfile.js
+++ gulpfile.js
@@ -8,40 +8,40 @@ import autoprefixer from "gulp-autoprefixer";
 import cleanCSS from "gulp-clean-css";
 import terser from "gulp-terser";
 import imagemin, { mozjpeg, optipng } from "gulp-imagemin";
-
 import webp from "gulp-webp";
+
 const sass = gulpSass(dartSass);
 
 function compileSass() {
-  return src("src/sass/*.sass")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(autoprefixer())
-    .pipe(cleanCSS())
-    .pipe(dest("dist/css"));
+	return src("src/sass/*.sass")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS())
+		.pipe(dest("./dist/"));
 }
 
 function jsMin() {
-  return src("src/js/*.js").pipe(terser()).pipe(dest("dist/js"));
+	return src("src/js/*.js").pipe(terser()).pipe(dest("dist/js"));
 }
 
 function optimizing() {
-  return src("src/images/*.{jpg,png}").pipe(
-    imagemin(
-      mozjpeg({ quality: 80, progressive: true }),
-      optipng({ optimizationLevel: 2 })
-    )
-  );
+	return src("src/images/*.{jpg,png}").pipe(
+		imagemin(
+			mozjpeg({ quality: 80, progressive: true }),
+			optipng({ optimizationLevel: 2 })
+		)
+	);
 }
 
 function webPImage() {
-  return src("src/images/*.{jpg,png}").pipe(webp()).pipe(dest("dist/images"));
+	return src("src/images/*.{jpg,png}").pipe(webp()).pipe(dest("dist/images"));
 }
 
 function watchTask() {
-  watch("src/sass/*.sass", compileSass);
-  watch("src/js/*.js", jsMin);
-  watch("src/images/*.{jpg,png}", optimizing);
-  watch("src/images/*.{jpg,png}", webPImage);
+	watch("src/sass/*.sass", compileSass);
+	// watch("src/js/*.js", jsMin);
+	// watch("src/images/*.{jpg,png}", optimizing);
+	// watch("src/images/*.{jpg,png}", webPImage);
 }
-
-export default series(compileSass, jsMin, optimizing, webPImage, watchTask);
+// jsMin, optimizing, webPImage,
+export default series(compileSass, watchTask);
